9ba1d628245c6350061d973093f38ba857da22b1
Ada Rose Edwards <ada@ada.is>
2016-04-05 14:44:01 -0700

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,12 @@ module.exports = {
 	module: {
 		loaders: [
 			{
-				test: /^js\/\.js?$/,
+				test: /\.js?$/,
 				exclude: /(node_modules|bower_components)/,
 				loader: 'babel',
 				query: {
-					presets: ['es2015']
+					presets: ['es2015'],
+					plugins: ['transform-runtime']
 				}
 			}
 		]
