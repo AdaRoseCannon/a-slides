185bee6b41c5e862c72db1c52e0c416cbceb9009
Ada Rose Edwards <ada@ada.is>
2016-07-27 18:53:42 +0100

    rebuild

--- js/events.js
+++ js/events.js
@@ -1,31 +1,32 @@
 
 'use strict';
 
-module.exports = {
-	on(node, name, fn) {
-		if (!node.funcRef) node.funcRef = new Set();
+function on(node, name, fn) {
+	if (!node.funcRef) node.funcRef = new Set();
 
-		// Store it for later
-		node.funcRef.add(fn);
-		node.addEventListener(name, fn);
-		return node;
-	},
+	// Store it for later
+	node.funcRef.add(fn);
+	node.addEventListener(name, fn);
+	return node;
+}
 
-	off(node, name, fn) {
-		if (!node.funcRef) return;
-		if (fn) {
-			node.removeEventListener(name, fn);
-		} else {
-			node.funcRef.forEach(fn => node.removeEventListener(name, fn));
-		}
-		node.funcRef.delete(fn);
-		return node;
-	},
+function off(node, name, fn) {
+	if (!node.funcRef) return;
+	if (fn) {
+		node.removeEventListener(name, fn);
+	} else {
+		node.funcRef.forEach(fn => node.removeEventListener(name, fn));
+	}
+	node.funcRef.delete(fn);
+	return node;
+}
+
+module.exports = {on, off,
 
 	once(node, name, fn) {
-		node.on(name, function tempF(e) {
+		on(node, name, function tempF(e) {
 			fn.bind(node)(e);
-			node.off(name, tempF);
+			off(node, name, tempF);
 		});
 		return node;
 	},
