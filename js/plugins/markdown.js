d2b53433881fdc857ff0ea397324339cb8eeb5c4
Ada Rose Edwards <ada@ada.is>
2016-07-27 18:41:32 +0100

    rebuild

--- js/plugins/markdown.js
+++ js/plugins/markdown.js
@@ -7,7 +7,7 @@ module.exports = function ({slideContainer}) {
 	const m = new Map();
 
 	// store all of the innerHTMLs
-	slideContainer.querySelector('.marked').forEach(o => m.set(o, o.innerHTML));
+	Array.from(slideContainer.querySelectorAll('.marked')).forEach(o => m.set(o, o.innerHTML));
 
 	// then write them all out
 	m.forEach((v, k) => k.innerHTML = marked(v));
